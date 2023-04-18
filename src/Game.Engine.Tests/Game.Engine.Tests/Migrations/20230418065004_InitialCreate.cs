f851e466592bff22b87b7d2d3401056113d19eca
parinya <parinya.ch@axa.co.th>
2023-04-18 13:57:21 +0700

    add SQLite EF example code

---
+++ src/Game.Engine.Tests/Game.Engine.Tests/Migrations/20230418065004_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Game.Engine.Tests.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "blogging");
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                schema: "blogging",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                schema: "blogging",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    BlogId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalSchema: "blogging",
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                schema: "blogging",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts",
+                schema: "blogging");
+
+            migrationBuilder.DropTable(
+                name: "Blogs",
+                schema: "blogging");
+        }
+    }
+}
---
+++ src/Game.Engine.Tests/Game.Engine.Tests/TestSQLiteEF.cs
@@ -0,0 +1,99 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Game.Engine.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using var db = new BloggingContext();
+            var blog = new Blog
+            {
+                Url = "test1"
+            };
+            db.Blogs.Add(blog);
+
+            blog.Posts.Add(new Post
+            {
+                Title = "555",
+                Content = "test",
+            });
+
+            blog.Posts.Add(new Post
+            {
+                Title = "555",
+                Content = "test",
+            });
+
+            db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestMethodRead()
+        {
+            using var db = new BloggingContext();
+            var blog = db.Blogs.Include(x => x.Posts).Single(x => x.Url == "test1");
+
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public string DbPath { get; }
+
+        public BloggingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.HasDefaultSchema("blogging");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Blog : DomainModel
+    {
+        public string Url { get; set; }
+
+        public List<Post>? Posts { get; } = new();
+    }
+
+    public class Post : DomainModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Guid BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+    public abstract class DomainModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+
+    public class Party
+    {
+        public string Name { get; set; }
+    }
+}
